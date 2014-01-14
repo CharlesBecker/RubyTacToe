30caedb3c81cf7ddf74b94b65d2fb0d2cf4886a1
Charles Becker <charleshbecker@gmail.com>
2014-01-14 11:01:42 -0700

    Create TextOutput.rb
    created TextOutput.rb

---
+++ TextOutput.rb
@@ -0,0 +1,47 @@
+class TextOutput
+
+	def initialize
+		@move_prompt = "Please enter a valid move (h = help)"
+	end
+	
+	def print_prompt prompt
+		print prompt + " "
+	end
+	
+	def print_move_prompt
+		print_prompt @move_prompt
+	end
+	
+	def test_output output
+		puts output
+	end
+	
+	def print_board master_list
+		line1 = '   |   |   '
+		line2 = '---|---|---'
+		puts line1
+		puts " " + master_list[0] + " | " + master_list[1] + " | " + master_list[2]
+		puts line1
+		puts line2
+		puts line1
+		puts " " + master_list[3] + " | " + master_list[4] + " | " + master_list[5]
+		puts line1
+		puts line2
+		puts line1
+		puts " " + master_list[6] + " | " + master_list[7] + " | " + master_list[8]
+		puts line1
+	end
+	
+	def game_won winner
+		puts "Congratulations, " + winner + "!  You won!"
+	end
+	
+	def game_stale
+		puts "No one won :("
+	end
+	
+	def game_quit
+		puts "Play again soon!"
+	end 
+	
+end
