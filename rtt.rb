074723a7410b7ec0a21e7db3bc0007ed34bfd12e
Charles Becker <charleshbecker@gmail.com>
2014-01-29 15:48:13 -0700

    Update rtt.rb
    updated main file.  Now has input validation and error displays.  Also fixed a few assorted bugs.

--- rtt.rb
+++ rtt.rb
@@ -3,29 +3,48 @@ require 'TextInput'
 require 'TextOutput'
 require 'TextValidater'
 
-game = Game.new
-input = TextInput.new
-output = TextOutput.new
+game 	  = Game.new
+input 	  = TextInput.new
+output 	  = TextOutput.new
 validater = TextValidater.new
 
 player_input = ''
+error 		 = ''
 
 output.print_board game.get_display
 
-while (player_input != '0' && game.get_status != false)
+while ((player_input != 'q') && (game.get_status != false))
+
+	if ((error != true) && (error != ''))
+		output.character_error
+	end
+
 	output.print_move_prompt
 	player_input = input.get_input
-	game.attempt_move player_input.to_i
-	output.print_board game.get_display
-	game.round_maintenance
-	
-	if    (game.get_status == false && game.is_won == true)
-		output.game_won(game.get_winner)
-	elsif (game.get_status == false && game.is_stale == true)
-		output.game_stale
-	elsif (game.get_status == false)
-		output.game_quit
+	error 		 = validater.move_test(player_input)
+
+	if ((error == true) && (player_input != 'q'))
+		error = game.attempt_move player_input.to_i
+		output.print_board game.get_display
+		
+		if (error == false)
+			output.taken_error
+		else
+			game.round_maintenance
+   
+			if    ((game.get_status == false) && (game.is_won   == true))
+					output.game_won(game.get_winner)
+			elsif ((game.get_status == false) && (game.is_stale == true))
+					output.game_stale
+			end
+   
+   			game.iterate_players
+		end
+		error = ''
+
 	end
 	
-	game.iterate_players
+	if (player_input == 'q')
+		output.game_quit
+	end	
 end
