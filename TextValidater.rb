a68d057b0c968af67c00a43c782ddf043d53ad32
Charles Becker <charleshbecker@gmail.com>
2014-01-29 15:48:52 -0700

    Update TextValidater.rb
    added methods to the TextValidater class
    Now functions as expected.

--- TextValidater.rb
+++ TextValidater.rb
@@ -1,2 +1,16 @@
 class TextValidater
+	
+	def initialize
+		@validInput = "123456789hq"
+	end
+	
+	def move_test player_input
+		if (@validInput.index(player_input))
+			result = true
+		else
+			result = "character error"
+		end
+		return result
+	end
+	
 end
